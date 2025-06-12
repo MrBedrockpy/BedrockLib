ad303531508716a29c2a6bd4162e402331187c5d
mrbedrockpy <MrBedrockpy@yandex.ru>
2025-06-12 20:04:40 +0300

    fix bugs

--- src/main/java/ru/mrbedrockpy/bedrocklib/db/DataBase.java
+++ src/main/java/ru/mrbedrockpy/bedrocklib/db/DataBase.java
@@ -10,9 +10,9 @@ import java.io.*;
 import java.util.*;
 
 @Getter
-public class DataBase<P extends BedrockPlugin> extends ListManager<P, DataTable<? extends ManagerItem<?>, ?>, Class<?>> {
+public class DataBase<P extends BedrockPlugin> extends ListManager<P, DataTable<P, ? extends ManagerItem<?>, ?>, Class<?>> {
 
-    private final List<DataTable<?, ?>> tables = new ArrayList<>();
+    private final List<DataTable<P, ?, ?>> tables = new ArrayList<>();
     private final SerializeConfig<? extends BedrockPlugin> serializeConfig;
     private final File file;
 
@@ -22,22 +22,22 @@ public class DataBase<P extends BedrockPlugin> extends ListManager<P, DataTable<
         this.serializeConfig = serializeConfig;
     }
 
-    public <T extends ManagerItem<ID>, ID> DataTable<T, ID> createTableIfNotExists(Class<T> dataType) {
-        DataTable<T, ID> table = getTable(dataType);
+    public <T extends ManagerItem<ID>, ID> DataTable<P, T, ID> createTableIfNotExists(Class<T> dataType) {
+        DataTable<P, T, ID> table = getTable(dataType);
         if (table != null) return table;
-        table = new DataTable<>(serializeConfig, dataType, List.of());
+        table = new DataTable<>(getPlugin(), serializeConfig, dataType, List.of());
         tables.add(table);
         return table;
     }
 
-    public <T extends ManagerItem<ID>, ID> DataTable<T, ID> getTable(Class<T> clazz) {
-        for (DataTable<?, ?> table: tables) {
-            if (table.getDataType().equals(clazz)) return (DataTable<T, ID>) table;
+    public <T extends ManagerItem<ID>, ID> DataTable<P, T, ID> getTable(Class<T> clazz) {
+        for (DataTable<P, ?, ?> table: tables) {
+            if (table.getDataType().equals(clazz)) return (DataTable<P, T, ID>) table;
         }
         return null;
     }
 
-    public List<DataTable<?, ?>> load() {
+    public List<DataTable<P, ?, ?>> load() {
         tables.clear();
         StringBuilder sb = new StringBuilder();
         try {
@@ -55,7 +55,7 @@ public class DataBase<P extends BedrockPlugin> extends ListManager<P, DataTable<
             String[] arrayTable = stringTable.split("\n");
             try {
                 Class<?> dataType = Class.forName(arrayTable[0]);
-                tables.add(new DataTable<>(serializeConfig, dataType, Arrays.asList(arrayTable).subList(1, arrayTable.length)));
+                tables.add(new DataTable<>(getPlugin(), serializeConfig, dataType, Arrays.asList(arrayTable).subList(1, arrayTable.length)));
             } catch (ClassNotFoundException e) {
                 throw new RuntimeException(e);
             }
@@ -67,7 +67,7 @@ public class DataBase<P extends BedrockPlugin> extends ListManager<P, DataTable<
         try {
             if (!file.exists()) if (!file.createNewFile()) throw new RuntimeException("Failed to create file: " + file.getAbsolutePath());
             List<String> stringTables = new ArrayList<>();
-            for (DataTable<?, ?> dataTable: tables) stringTables.add(dataTable.getDataType().getName() + "\n" + String.join("\n", dataTable.serialize()));
+            for (DataTable<P, ?, ?> dataTable: tables) stringTables.add(dataTable.getDataType().getName() + "\n" + String.join("\n", dataTable.serialize()));
             FileWriter fw = new FileWriter(file);
             fw.write(Base64.getEncoder().encodeToString(String.join("\n\n", stringTables).getBytes()));
             fw.close();
